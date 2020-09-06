0748db191c76951c196fff4de1ffc9ba0aa68c5e
ROHIT <rrathour.rohit23@gmail.com>
2020-09-06 13:14:05 +0530

    FirstCommit(6Sept2020

---
+++ src/main/java/com/demo/completeLoginProject/LoginWithJwtAndOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.demo.completeLoginProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithJwtAndOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithJwtAndOAuthApplication.class, args);
+	}
+
+}
